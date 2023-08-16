d3b1266eb560bb4ddf32a922ed9383d815ec5cf5
molmax <molmax@users.noreply.github.com>
2023-08-16 22:34:17 +0300

    Setting up the field

--- src/main.js
+++ src/main.js
@@ -1,10 +1,71 @@
-canvasWidth = 800;
-canvasHeight = 800;
+canvasWidth = 600;
+canvasHeight = 600;
+
+let cells = [];
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
+  initCells();
 }
 
 function draw() {
-  background(20, 20, 30);
+  cells.forEach(cell => {
+    cell.draw();
+  });
+}
+
+function initCells() {
+  cellSize = 10;
+  for (let y = 0; y < canvasHeight; y += cellSize) {
+    for (let x = 0; x < canvasWidth; x += cellSize) {
+        prob = monteCarlo();
+        chance = 350;
+        isAlive = Math.round(random(prob)) > chance;
+        cells.push(new Cell(x, y, cellSize, isAlive));
+    }
+  }
+}
+
+function monteCarlo() {
+  while (true) {
+    let r1 = random(-600, 600);
+    let probability = r1;
+    let r2 = random(-600, 600);
+
+    if (r2 < probability) {
+      return r1;
+    }
+  }
+}
+
+class Cell {
+  x;
+  y;
+  size;
+  isAlive;
+
+  constructor(x, y, size, isAlive) {
+    this.x = x;
+    this.y = y; 
+    this.size = size;
+    this.isAlive = isAlive;
+  }
+
+  makeAlive() {
+    this.isAlive = true;
+  }
+
+  kill() {
+    this.isAlive = false;
+  }
+
+  draw() {
+    if (this.isAlive) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    noStroke();
+    square(this.x, this.y, this.size);
+  }
 }
